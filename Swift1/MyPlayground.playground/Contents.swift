efb897963c83554cfa33e153bc409dfe91e7c349
LinHK <LHK@kuangshichuanqideMacBook-Pro.local>
2018-03-01 21:34:54 +0800

    test

---
+++ Swift1/MyPlayground.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var offset = 2008 % 12
+( 2017 - offset) % 12
+		
--- Swift1/ViewController.swift
+++ Swift1/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var yeartext: UITextField!
+    @IBOutlet weak var imageV: UIImageView!
+    
+    var offet = 2008 % 12
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +26,24 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        yeartext.resignFirstResponder()
+    }
 
-
+    @IBAction func btnClick(_ sender: Any) {
+        yeartext.resignFirstResponder()
+        let string1 = yeartext.text!
+        if  string1.isEmpty{
+            print("不要输入空")
+            return
+        }
+        let year = Int(yeartext.text!)!
+        if year > 0{
+            let imageStr = (year - offet ) % 12
+            imageV.image = UIImage(named:String(imageStr))
+        }
+        
+    }
 }
 
